d9fd10a622893eeba516a97156ec4cf887bab7e7
Sean Hogge <seanhogge@users.noreply.github.com>
2023-05-09 17:16:56 -0500

    Add policy

---
+++ app/policies/attachment_policy.rb
@@ -0,0 +1,34 @@
+class AttachmentPolicy < ApplicationPolicy
+  # https://github.com/varvet/pundit
+  # See ApplicationPolicy for defaults
+
+  def destroy?
+    user == record.storage_unit.user || user.admin?
+  end
+
+  def permitted_attributes
+    if user == record.storage_unit.user || user.admin?
+      [
+        :id,
+        :storage_unit_id,
+        :images,
+      ]
+    else
+      [
+        :id,
+        :storage_unit_id,
+        :images,
+      ]
+    end
+  end
+
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(user: user)
+      end
+    end
+  end
+end
