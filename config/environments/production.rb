f60ee1f5eba6f41c6bc5b2b489aca6fbf42d2cda
Sean Hogge <seanhogge@users.noreply.github.com>
2023-05-10 08:38:05 -0500

    Fix token

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = {
-    api_token: Rails.application.credentials.postmark_api_token
+    api_token: Rails.application.credentials.production.postmark_api_token
   }
   config.action_mailer.default_url_options = { host: "https://#{Rails.application.credentials.dig(:production, :domain)}"  }
   config.action_mailer.raise_delivery_errors = true
